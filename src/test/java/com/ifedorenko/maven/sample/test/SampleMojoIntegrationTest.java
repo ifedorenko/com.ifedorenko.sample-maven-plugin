806a91821d2c195e5573469396ba78d87804ffbf
Igor Fedorenko <igor@ifedorenko.com>
2014-07-03 11:32:51 +0400

    basic integration test with reactor propagation
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ src/test/java/com/ifedorenko/maven/sample/test/SampleMojoIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.ifedorenko.maven.sample.test;
+
+import io.takari.maven.testing.TestResources;
+import io.takari.maven.testing.it.VerifierResult;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class SampleMojoIntegrationTest extends AbstractMojoIntegrationTest {
+
+  public SampleMojoIntegrationTest(String version) throws Exception {
+    super(version);
+  }
+
+  @Test
+  public void testBasic() throws Exception {
+    File basedir = resources.getBasedir("basic");
+
+    VerifierResult result = verifier.forProject(basedir).execute("compile");
+
+    result.assertErrorFreeLog();
+    TestResources.assertFilesPresent(basedir, "target/sample/sample.txt");
+  }
+
+}

8393978c320133e4f3bb4f8bd711cd0328a2c01f
Igor Fedorenko <igor@ifedorenko.com>
2015-01-22 10:32:19 -0500

    takari lifecycle 1.10.2, plugin testing 2.1.0
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- src/test/java/com/ifedorenko/maven/sample/test/SampleMojoIntegrationTest.java
+++ src/test/java/com/ifedorenko/maven/sample/test/SampleMojoIntegrationTest.java
@@ -2,10 +2,9 @@ package com.ifedorenko.maven.sample.test;
 
 import io.takari.maven.testing.TestResources;
 import io.takari.maven.testing.executor.MavenExecutionResult;
-import io.takari.maven.testing.executor.MavenInstallations;
 import io.takari.maven.testing.executor.MavenRuntime;
-import io.takari.maven.testing.executor.MavenVersions;
 import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
+import io.takari.maven.testing.executor.MavenVersions;
 import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
 
 import java.io.File;
@@ -15,8 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(MavenJUnitTestRunner.class)
-@MavenInstallations({"target/maven-installation/apache-maven-3.2.2"})
-@MavenVersions({"3.0.5", "3.2.3"})
+@MavenVersions({"3.0.5", "3.2.5"})
 public class SampleMojoIntegrationTest {
 
   @Rule
--- src/test/java/com/ifedorenko/maven/sample/test/SampleMojoTest.java
+++ src/test/java/com/ifedorenko/maven/sample/test/SampleMojoTest.java
@@ -1,11 +1,10 @@
 package com.ifedorenko.maven.sample.test;
 
-import static org.apache.maven.plugin.testing.resources.TestResources.assertDirectoryContents;
+import io.takari.maven.testing.TestMavenRuntime;
+import io.takari.maven.testing.TestResources;
 
 import java.io.File;
 
-import org.apache.maven.plugin.testing.MojoRule;
-import org.apache.maven.plugin.testing.resources.TestResources;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -15,12 +14,12 @@ public class SampleMojoTest {
   public TestResources resources = new TestResources();
 
   @Rule
-  public MojoRule mojos = new MojoRule();
+  public TestMavenRuntime mojos = new TestMavenRuntime();
 
   @Test
   public void testBasic() throws Exception {
     File basedir = resources.getBasedir("basic");
     mojos.executeMojo(basedir, "sample");
-    assertDirectoryContents(new File(basedir, "target/sample"), "sample.txt");
+    TestResources.assertDirectoryContents(new File(basedir, "target/sample"), "sample.txt");
   }
 }
